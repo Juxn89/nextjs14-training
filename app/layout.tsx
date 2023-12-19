887e71a49cb83adb36482e0f0fe3ae0df6342499
Juan Gómez <gomezj01@stepstone.com>
2023-12-19 11:13:29 -0600

    Rutas, rutas anidadas y layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={ `${ montserrat.className } antialiased` }>{children}</body>
+      <footer>
+        <span>This footer is in Layout</span>
+      </footer>
     </html>
   );
 }
